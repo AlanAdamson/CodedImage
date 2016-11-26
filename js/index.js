54a74095c2b8237b05edb34d3a05f13c7baea526
AlanAdamson <alan@adamson.im>
2016-11-25 18:44:38 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,2 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var massive = require('massive');
-var app = module.exports = express();
-app.use(bodyParser.json());
-app.use(cors());
-
 var addition = 1 + 1;
-console.log(addition);
+console.log('Addition test ran and 1+1=' + addition);
