4cafb950f9236bdd2bd4f8c2f1657970b09e798c
AlanAdamson <alan@adamson.im>
2016-11-25 20:03:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var express = require('express');
 var app = module.exports = express();
 var path = require('path');
-var assets = require('connect-assets');
-
-app.use(assets({
-  paths: [
-    'assets/css',
-    'assets/js',
-    'assets/img'
-  ]
-}));
 
 var addition = 1 + 1;
 console.log('Addition test ran and 1+1=' + addition);
