4aaf5d7f0f91832b44d2212bcd9a029b0bbbccdd
AlanAdamson <alan@adamson.im>
2016-11-25 19:23:41 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = module.exports = express();
+var path = require('path');
 
 var addition = 1 + 1;
 console.log('Addition test ran and 1+1=' + addition);
 
 app.get('/', function(req, res) {
-    res.render('../index.html');
+    res.sendFile(path.join(__dirname + '../index.html');
 });
 
 app.listen(process.env.PORT || 3000, function(){
