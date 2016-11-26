faa42b9c26720c83b0a715a53a30028beb73e992
AlanAdamson <alan@adamson.im>
2016-11-25 18:24:38 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-//intentially left blank
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var massive = require('massive');
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',
+  );
