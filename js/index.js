14a2d0832cb634f5d089ea75366b00ec72cda485
AlanAdamson <alan@adamson.im>
2016-11-25 19:19:19 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,9 @@ var app = module.exports = express();
 var addition = 1 + 1;
 console.log('Addition test ran and 1+1=' + addition);
 
-app.get('/', index.html);
+app.get('/', function(req, res) {
+    res.render('../index.html');
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
