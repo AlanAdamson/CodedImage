8e3c12d67be01acb130030a9912e7e9b9169d4c9
AlanAdamson <alan@adamson.im>
2016-11-25 19:47:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = module.exports = express();
 var path = require('path');
+var assets = require('connect-assets');
+
+app.use(assets({
+  paths: [
+    'assets/css',
+    'assets/js',
+    'assets/img'
+  ]
+}));
 
 var addition = 1 + 1;
 console.log('Addition test ran and 1+1=' + addition);
@@ -8,6 +17,8 @@ console.log('Addition test ran and 1+1=' + addition);
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
