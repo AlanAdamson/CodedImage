a56f857ff3f716be34020cf7c6f583c026aeed91
AlanAdamson <alan@adamson.im>
2017-01-14 09:42:29 -0700

    Update mainCtrl.js

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -3,7 +3,10 @@ angular.module('mainApp').controller('mainCtrl', function($scope, mainServ, $loc
   $scope.test = "working";
 
   $scope.currentURL = function($location.url) {
+    console.log('$location.url: ' + $location.url);
     var local = $location.url;
+    console.log('local: ' + local);
+    console.log('local cut: ' + local.substr(0, path.length - 1));
     return local.substr(0, path.length - 1);
   }
 
