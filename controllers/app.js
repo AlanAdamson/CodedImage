f22aed4e2967c94126546402399dfb90ec568094
AlanAdamson <alan@adamson.im>
2017-01-14 07:02:14 -0700

    Update app.js

--- controllers/app.js
+++ controllers/app.js
@@ -4,10 +4,11 @@ angular.module('mainApp', ['ui.router'])
 
   $stateProvider
 
-//   .state('home', {
-//     url: '/',
-//     templateUrl: './views/about.html'
-//   })
+  .state('home', {
+    url: '/',
+    templateUrl: './views/home.html',
+    redirectTo: 'about'
+  })
 
   .state('about', {
     url: '/about',
@@ -25,5 +26,14 @@ angular.module('mainApp', ['ui.router'])
   });
 
   $locationProvider.html5Mode(true);
+  
+  .run(['$rootScope', '$state', function($rootScope, $state) {
+
+    $rootScope.$on('$stateChangeStart', function(evt, to, params) {
+      if (to.redirectTo) {
+        evt.preventDefault();
+        $state.go(to.redirectTo, params, {location: 'replace'})
+      }
+    });
   
 });
