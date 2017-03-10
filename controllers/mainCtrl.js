6eddafbafd951e8022f5465cab4947ef94222e14
AlanAdamson <alan@adamson.im>
2017-03-10 15:50:07 -0700

    added angular test to confirm suspiciouns

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -5,9 +5,7 @@ angular.module('mainApp').controller('mainCtrl', function($scope, mainServ, $loc
   var local = $location.url();
   $scope.currentURL = getLocal(local);
   function getLocal(local) {
-    console.log('local: ' + local);
-    console.log('local cut: ' + local.slice(1, local.length + 1));
     return local.slice(1, local.length + 1);
-  };
+  }
 
 });
